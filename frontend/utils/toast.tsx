05711f977ca18edc8671a6bdd6c6bd2179bf74f7
Daniel Kohari <48031948+danielkoh99@users.noreply.github.com>
2025-05-06 23:33:09 +0200

    add default timeout to toast message

--- frontend/utils/toast.tsx
+++ frontend/utils/toast.tsx
@@ -3,7 +3,12 @@ import { ReactNode } from "react";
 
 const createToast =
   (color: "success" | "danger" | "primary" | "warning") =>
-  (title: string, message: string, endContent?: ReactNode, timeout?: number) =>
+  (
+    title: string,
+    message: string,
+    endContent?: ReactNode,
+    timeout: number = 1000,
+  ) =>
     addToast({ title, description: message, color, endContent, timeout });
 
 const toast = {
